7e311d9a48ad34661fa6e6463fc33a175a159ad3
Molean <cnmolean@gmail.com>
2025-02-13 17:02:49 +0800

    1

--- src/main/java/com/molean/folia/adapter/FoliaUtils.java
+++ src/main/java/com/molean/folia/adapter/FoliaUtils.java
@@ -1,20 +1,26 @@
 package com.molean.folia.adapter;
 
+import com.google.common.collect.Iterables;
 import io.papermc.paper.threadedregions.TickRegionScheduler;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.Entity;
-import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public class FoliaUtils {
 
     public static Iterable<Entity> getLocalEntities() {
-        return TickRegionScheduler.getCurrentRegionizedWorldData().getLocalEntities();
+        return Iterables.transform(TickRegionScheduler.getCurrentRegionizedWorldData().getLocalEntities(), net.minecraft.world.entity.Entity::getBukkitEntity);
     }
 
     public static List<Player> getLocalPlayers() {
         return TickRegionScheduler.getCurrentRegionizedWorldData().getLocalPlayers().stream().map(ServerPlayer::getBukkitEntity).map(craftPlayer -> (Player) craftPlayer).toList();
+    }
+
+    public static CompletableFuture<Boolean> teleportAsync(Entity entity, Location location) {
+        return entity.teleportAsync(location);
     }
 }
